24a70e38a25a04aad00a2d20e8389fca7b9d261c
dsua <ys@ds-ua.com>
2013-01-21 23:32:58 +0200

    +moveOrder +moveOrderSpot +moveOrderFO +getRowFromTable change sendLimit,sendRPS,sendMarket,sendReportOnRPS,killOrder,toPrice

--- QL.lua
+++ QL.lua
@@ -1,8 +1,13 @@
--- Version 0.2
+-- Version 0.3
+--[[
+	Добавили moveOrder,moveOrderSpot,moveOrderFO. Вынесли список срочных классов - FUT_OPT_CLASSES. Изменили порядок входящих параметров killOrder и необходимое их минимальное количество.
+	Изменили количество входных параметров в toPrice. Добавили функцию getRowFromTable. Изменили исходящие пароаметры sendLimit, sendMarket, sendRPS, sendReportOnRPS
+]]--
 package.cpath=".\\?.dll;.\\?51.dll;C:\\Program Files (x86)\\Lua\\5.1\\?.dll;C:\\Program Files (x86)\\Lua\\5.1\\?51.dll;C:\\Program Files (x86)\\Lua\\5.1\\clibs\\?.dll;C:\\Program Files (x86)\\Lua\\5.1\\clibs\\?51.dll;C:\\Program Files (x86)\\Lua\\5.1\\loadall.dll;C:\\Program Files (x86)\\Lua\\5.1\\clibs\\loadall.dll;C:\\Program Files\\Lua\\5.1\\?.dll;C:\\Program Files\\Lua\\5.1\\?51.dll;C:\\Program Files\\Lua\\5.1\\clibs\\?.dll;C:\\Program Files\\Lua\\5.1\\clibs\\?51.dll;C:\\Program Files\\Lua\\5.1\\loadall.dll;C:\\Program Files\\Lua\\5.1\\clibs\\loadall.dll"..package.cpath
 package.path=package.path..";.\\?.lua;C:\\Program Files (x86)\\Lua\\5.1\\lua\\?.lua;C:\\Program Files (x86)\\Lua\\5.1\\lua\\?\\init.lua;C:\\Program Files (x86)\\Lua\\5.1\\?.lua;C:\\Program Files (x86)\\Lua\\5.1\\?\\init.lua;C:\\Program Files (x86)\\Lua\\5.1\\lua\\?.luac;C:\\Program Files\\Lua\\5.1\\lua\\?.lua;C:\\Program Files\\Lua\\5.1\\lua\\?\\init.lua;C:\\Program Files\\Lua\\5.1\\?.lua;C:\\Program Files\\Lua\\5.1\\?\\init.lua;C:\\Program Files\\Lua\\5.1\\lua\\?.luac;"
 require"bit"
 require"socket"
+FUT_OPT_CLASSES="FUTUX,OPTUX,SPBOPT,SPBFUT"
 --require"iuplua"
 --require"iupluacontrols"
 function sendLimit(class,security,direction,price,volume,account,client_code,comment)
@@ -10,12 +15,11 @@ function sendLimit(class,security,direction,price,volume,account,client_code,com
 	-- все параметры кроме кода клиента и коментария должны быть не нил
 	-- ВАЖНО! цена должна быть стрингом с количеством знаков после точки для данной бумаги
 	-- если код клиента нил - подлставляем счет
-	-- Данная функция возвращает 3 параметра 
-	--     1. Результат проверки заявки сервером Квик (true\false)
-	--     2. ID присвоенный транзакции
-	--     3. Ответноге сообщение сервера Квик либо строку с параметрами транзакции
+	-- Данная функция возвращает 2 параметра 
+	--     1. ID присвоенный транзакции либо nil если транзакция отвергнута на уровне сервера Квик
+	--     2. Ответное сообщение сервера Квик либо строку с параметрами транзакции
 	if (class==nil or security==nil or direction==nil or price==nil or volume==nil or account==nil) then
-		return false,0,"QL.sendLimit(): Can`t send order. Nil parameters."
+		return nil,"QL.sendLimit(): Can`t send order. Nil parameters."
 	end
 	local trans_id=math.random(2000000000)
 	local transaction={
@@ -38,23 +42,21 @@ function sendLimit(class,security,direction,price,volume,account,client_code,com
 	end
 	local res=sendTransaction(transaction)
 	if res~="" then
-		return false,trans_id, "QL.sendLimit():"..res
+		return nil, "QL.sendLimit():"..res
 	else
-		return true,trans_id, "QL.sendLimit(): Limit order sended sucesfully. Class="..class.." Sec="..security.." Dir="..direction.." Price="..price.." Vol="..volume.." Acc="..account.." Trans_id="..trans_id
+		return trans_id, "QL.sendLimit(): Limit order sended sucesfully. Class="..class.." Sec="..security.." Dir="..direction.." Price="..price.." Vol="..volume.." Acc="..account.." Trans_id="..trans_id
 	end
 end
 function sendMarket(class,security,direction,volume,account,client_code,comment)
 	-- отправка рыночной заявки
 	-- все параметры кроме кода клиента и коментария должны быть не нил
 	-- если код клиента нил - подлставляем счет
-	-- Данная функция возвращает 3 параметра 
-	--     1. Результат проверки заявки сервером Квик (true\false)
-	--     2. ID присвоенный транзакции
-	--     3. Ответноге сообщение сервера Квик либо строку с параметрами транзакции
+	-- Данная функция возвращает 2 параметра 
+	--     1. ID присвоенный транзакции либо nil если транзакция отвергнута на уровне сервера Квик
+	--     2. Ответное сообщение сервера Квик либо строку с параметрами транзакции
 	if (class==nil or security==nil or direction==nil  or volume==nil or account==nil) then
-		return false,0,"QL.sendMarket(): Can`t send order. Nil parameters."
+		return nil,"QL.sendMarket(): Can`t send order. Nil parameters."
 	end
-	local FUT_OPT_CLASSES="FUTUX,OPTUX,SPBOPT,SPBFUT"
 	local trans_id=math.random(2000000000)
 	local transaction={
 		["TRANS_ID"]=tostring(trans_id),
@@ -85,21 +87,174 @@ function sendMarket(class,security,direction,volume,account,client_code,comment)
 	end
 	local res=sendTransaction(transaction)
 	if res~="" then
-		return false,trans_id, "QL.sendMarket():"..res
+		return nil, "QL.sendMarket():"..res
 	else
-		return true,trans_id, "QL.sendMarket(): Market order sended sucesfully. Class="..class.." Sec="..security.." Dir="..direction.." Vol="..volume.." Acc="..account.." Trans_id="..trans_id
+		return trans_id, "QL.sendMarket(): Market order sended sucesfully. Class="..class.." Sec="..security.." Dir="..direction.." Vol="..volume.." Acc="..account.." Trans_id="..trans_id
 	end
 end
-function moveOrders()
+function moveOrder(mode,fo_number,fo_p,fo_q,so_number,so_p,so_q)
 	-- перемещение заявки
-	-- ВНИМАНИЕ! Доступна только на срочной секции (FORTS,FOUX)
+	-- минимальный набор параметров mode,fo_number,fo_p
+	-- в зависимости от класса первой заявки будет вызвана функция перемещения либо для СПОТ либо Срочного рынка
+	if (fo_number==nil or fo_p==nil or mode==nil) then
+		return nil,"QL.moveOrder(): Can`t move order. Nil parameters."
+	end
+	local forder=getRowFromTable("orders","ordernum",fo_number)
+	if string.find(FUT_OPT_CLASSES,forder.class_code)~=nil then
+		return moveOrderFO(mode,fo_number,fo_p,fo_q,so_number,so_p,so_q)
+	else
+		return moveOrderSPOT(mode,fo_number,fo_p,fo_q,so_number,so_p,so_q)
+	end
+end
+function moveOrderSpot(mode,fo_number,fo_p,fo_q,so_number,so_p,so_q)
+	-- перемещение заявок для рынка спот
+	-- минимальный набор параметров mode,fo_number,fo_p
+	-- отправится 2 транзакции снятие+постановка для каждой из указанных заявок
+	-- Возвращаем 2 параметра :
+	-- 1. Nil - если неудача или номер транзакции (2-й если 2 заявки)
+	-- 2. Диагностиеское сообщение
+	if (order_number==nil or new_price==nil or regim==nil) then
+		return nil,"QL.moveOrderSpot(): Can`t move order. Nil parameters."
+	end
+	local forder=getRowFromTable("orders","ordernum",fo_number)
+	if (orderflags2table(forder.flags).cancelled==1 or (orderflags2table(forder.flags).done==1 and forder.balance==0)) then
+		return nil,"QL.moveOrderSpot(): Can`t move cancelled or done order!"
+	end
+	if mode==0 then
+		--Если MODE=0, то заявки с номерами, указанными после ключей FIRST_ORDER_NUMBER и SECOND_ORDER_NUMBER, снимаются. 
+		--В торговую систему отправляются две новые заявки, при этом изменяется только цена заявок, количество остается прежним;
+		if so_number~=nil and so_p~=nil then
+			_,_=killOrder(fo_number,forder.seccode,forder.class_code)
+			trid,ms1=sendLimit(forder.class_code,forder.seccode,orderflags2table(forder.flags).operation,fo_p,forder.qty,forder.account,forder.client_code,forder.comment)
+			local sorder=getRowFromTable("orders","ordernum",so_number)
+			_,_=killOrder(so_number,sorder.seccode,sorder.class_code)
+			trid2,ms2=sendLimit(sorder.class_code,sorder.seccode,orderflags2table(sorder.flags).operation,so_p,sorder.qty,sorder.account,sorder.client_code,sorder.comment)
+			if trid~=nil and trid2~=nil then
+				return trid2,"QL.moveOrderSpot(): Orders moved. Trans_id1="..trid.." Trans_id2="..trid2
+			else
+				return nil,"QL.moveOrderSpot(): One or more orders not moved! Msg1="..ms1.." Msg2="..ms2
+			end
+		else
+			_,_=killOrder(fo_number,forder.seccode,forder.class_code)
+			return sendLimit(forder.class_code,forder.seccode,orderflags2table(forder.flags).operation,fo_p,forder.qty,forder.account,forder.client_code,forder.comment)
+		end
+	elseif mode==1 then
+		--Если MODE=1, то заявки с номерами, указанными после ключей FIRST_ORDER_NUMBER и SECOND_ORDER_NUMBER, снимаются. 
+		--В торговую систему отправляются две новые заявки, при этом изменится как цена заявки, так и количество;
+		if so_number~=nil and so_p~=nil and so_q~=nil then
+			_,_=killOrder(fo_number,forder.seccode,forder.class_code)
+			local trid,ms1=sendLimit(forder.class_code,forder.seccode,orderflags2table(forder.flags).operation,fo_p,fo_q,forder.account,forder.client_code,forder.comment)
+			local sorder=getRowFromTable("orders","ordernum",so_number)
+			_,_=killOrder(so_number,sorder.seccode,sorder.class_code)
+			local trid2,ms2=sendLimit(sorder.class_code,sorder.seccode,orderflags2table(sorder.flags).operation,so_p,so_q,sorder.account,sorder.client_code,sorder.comment)
+			if trid~=nil and trid2~=nil then
+				return trid2,"QL.moveOrderSpot(): Orders moved. Trans_id1="..trid.." Trans_id2="..trid2
+			else
+				return nil,"QL.moveOrderSpot(): One or more orders not moved! Msg1="..ms1.." Msg2="..ms2
+			end
+		else
+			_,_=killOrder(fo_number,forder.seccode,forder.class_code)
+			local trid,ms=sendLimit(forder.class_code,forder.seccode,orderflags2table(forder.flags).operation,fo_p,fo_q,forder.account,forder.client_code,forder.comment)
+			if trid~=nil then
+				return trid,"QL.moveOrderSpot(): Order moved. Trans_Id="..trid
+			else
+				return nil,"QL.moveOrderSpot(): Order not moved! Msg="..ms
+			end
+		end
+	elseif mode==2 then
+		--Если MODE=2,  то заявки с номерами, указанными после ключей FIRST_ORDER_NUMBER и SECOND_ORDER_NUMBER, снимаются. 
+		--Если количество бумаг в каждой из снятых заявок совпадает со значениями, указанными после FIRST_ORDER_NEW_QUANTITY и SECOND_ORDER_NEW_QUANTITY, то в торговую систему отправляются две новые заявки с соответствующими параметрами.
+		if so_number~=nil and so_p~=nil and so_q~=nil then
+			local sorder=getRowFromTable("orders","ordernum",so_number)
+			_,_=killOrder(fo_number,forder.seccode,forder.class_code)
+			_,_=killOrder(so_number,sorder.seccode,sorder.class_code)
+			if forder.balance==fo_q and sorder.balance==so_q then
+				local trid,ms1=sendLimit(forder.class_code,forder.seccode,orderflags2table(forder.flags).operation,fo_p,fo_q,forder.account,forder.client_code,forder.comment)
+				local trid2,ms2=sendLimit(sorder.class_code,sorder.seccode,orderflags2table(sorder.flags).operation,so_p,so_q,sorder.account,sorder.client_code,sorder.comment)
+				if trid~=nil and trid2~=nil then
+					return trid2,"QL.moveOrderSpot(): Orders moved. Trans_id1="..trid.." Trans_id2="..trid2
+				else
+					return nil,"QL.moveOrderSpot(): One or more orders not moved! Msg1="..ms1.." Msg2="..ms2
+				end
+			else
+				return nil,"QL.moveOrderSpot(): Mode=2. Orders balance~=new_quantity"
+			end
+		else
+			_,_=killOrder(fo_number,forder.seccode,forder.class_code)
+			local trid,ms=sendLimit(forder.class_code,forder.seccode,orderflags2table(forder.flags).operation,fo_p,fo_q,forder.account,forder.client_code,forder.comment)
+			if trid~=nil then
+				return trid,"QL.moveOrderSpot(): Order moved. Trans_Id="..trid
+			else
+				return nil,"QL.moveOrderSpot(): Order not moved! Msg="..ms
+			end
+		end
+	else
+		return nil,"QL.moveOrder(): Mode out of range! Mode can be from {0,1,2}"
+	end
+end
+function moveOrderFO(mode,fo_number,fo_p,fo_q,so_number,so_p,so_q)
+	-- перемещение заявок для срочного рынка
+	-- отправка "нормальной" транзакции Квика
+	if (fo_number==nil or fo_p==nil or mode==nil) then
+		return nil,"QL.moveOrderFO(): Can`t move order. Nil parameters."
+	end
+	local transaction={}
+	if mode==0 then
+		if so_number~=nil and so_p~=nil then
+			transaction["SECOND_ORDER_NUMBER"]=tostring(so_number)
+			transaction["SECOND_ORDER_NEW_PRICE"]=so_p
+			transaction["SECOND_ORDER_NEW_QUANTITY"]="0"
+		end
+		transaction["FIRST_ORDER_NUMBER"]=tostring(fo_number)
+		transaction["FIRST_ORDER_NEW_PRICE"]=fo_p
+		transaction["FIRST_ORDER_NEW_QUANTITY"]="0"
+	elseif mode==1 then
+		if fo_q==nil or fo_q==0 then
+			return nil,"QL.moveOrder(): Mode=1. First Order Quantity can`t be nil or zero!"
+		end
+		if so_number~=nil and so_p~=nil and so_q>0 then
+			transaction["SECOND_ORDER_NUMBER"]=tostring(so_number)
+			transaction["SECOND_ORDER_NEW_PRICE"]=so_p
+			transaction["SECOND_ORDER_NEW_QUANTITY"]=tostring(so_q)
+		end
+		transaction["FIRST_ORDER_NUMBER"]=tostring(fo_number)
+		transaction["FIRST_ORDER_NEW_PRICE"]=fo_p
+		transaction["FIRST_ORDER_NEW_QUANTITY"]=tostring(fo_q)
+	elseif mode==2 then
+		if fo_q==nil or fo_q==0 then
+			return nil,"QL.moveOrder(): Mode=2. First Order Quantity can`t be nil or zero!"
+		end
+		if so_number~=nil and so_p~=nil and so_q>0 then
+			transaction["SECOND_ORDER_NUMBER"]=tostring(so_number)
+			transaction["SECOND_ORDER_NEW_PRICE"]=so_p
+			transaction["SECOND_ORDER_NEW_QUANTITY"]=tostring(so_q)
+		end
+		transaction["FIRST_ORDER_NUMBER"]=tostring(fo_number)
+		transaction["FIRST_ORDER_NEW_PRICE"]=fo_p
+		transaction["FIRST_ORDER_NEW_QUANTITY"]=tostring(fo_q)
+	else
+		return nil,"QL.moveOrder(): Mode out of range! mode can be from {0,1,2}"
+	end
+	local trans_id=math.random(2000000000)
+	local order=getRowFromTable("orders","ordernum",fo_number)
+	local transaction={
+		["TRANS_ID"]=tostring(trans_id),
+		["CLASSCODE"]=order.class_code,
+		["SECCODE"]=order.seccode,
+		["ACTION"]="MOVE_ORDERS"
+	}
+	local res=sendTransaction(transaction)
+	if res~="" then
+		return nil, "QL.moveOrderFO():"..res
+	else
+		return trans_id, "QL.moveOrderFO(): Market order sended sucesfully. Mode="..mode.." FONumber="..fo_number.." FOPrice="..fo_p
+	end
 end
 function sendRPS(class,security,direction,price,volume,account,client_code,partner)
     -- функция отправки заявки на внебиржевую сделку
 	if (class==nil or security==nil or direction==nil or price==nil or volume==nil or account==nil or partner==nil) then
-		return false,"QL.sendRPS(): Can`t send order. Nil parameters."
+		return nil,"QL.sendRPS(): Can`t send order. Nil parameters."
 	end
-	--local trans_id=tostring(math.ceil(os.clock()))..tostring(math.random(os.clock()))
 	local trans_id=math.random(2000000000)
 	local transaction={
 		["TRANS_ID"]=tostring(trans_id),
@@ -120,15 +275,15 @@ function sendRPS(class,security,direction,price,volume,account,client_code,partn
 	end
 	local res=sendTransaction(transaction)
 	if res~="" then
-		return false,trans_id, "QL.sendRPS():"..res
+		return nil, "QL.sendRPS():"..res
 	else
-		return true,trans_id, "QL.sendRPS(): RPS order sended sucesfully. Class="..class.." Sec="..security.." Dir="..direction.." Price="..price.." Vol="..volume.." Acc="..account.." Partner="..partner.." Trans_id="..trans_id
+		return trans_id, "QL.sendRPS(): RPS order sended sucesfully. Class="..class.." Sec="..security.." Dir="..direction.." Price="..price.." Vol="..volume.." Acc="..account.." Partner="..partner.." Trans_id="..trans_id
 	end
 end
 function sendReportOnRPS(class,operation,key)
     -- отправка отчета по сделки для исполнения
 	if(class==nil or operation==nil or key==nil) then
-		return false,"QL.sendRPS(): Can`t send order. Nil parameters."
+		return nil,"QL.sendRPS(): Can`t send order. Nil parameters."
 	end
 	--local trans_id=tostring(math.ceil(os.clock()))..tostring(math.random(os.clock()))
 	local trans_id=math.random(2000000000)
@@ -141,33 +296,40 @@ function sendReportOnRPS(class,operation,key)
 	}
 	local res=sendTransaction(transaction)
 	if res~="" then
-		return false,trans_id, "QL.sendReportOnRPS():"..res
+		return nil, "QL.sendReportOnRPS():"..res
 	else
-		return true,trans_id, "QL.sendReportOnRPS(): ReportOnRPS order sended sucesfully. Class="..class.." Oper="..operation.." Key="..key.." Trans_id="..trans_id
+		return trans_id, "QL.sendReportOnRPS(): ReportOnRPS order sended sucesfully. Class="..class.." Oper="..operation.." Key="..key.." Trans_id="..trans_id
 	end
 end
-function killOrder(class,security,orderkey)
-	
+function killOrder(orderkey,security,class)
 	-- функция отмены лимитированной заявки по номеру
+	-- принимает минимум 1 парамер
 	-- ВАЖНО! Данная функция не гарантирует снятие заявки
 	-- Возвращает сообщение сервера в случае ошибки выявленной сервером Квик либо строку с информацией о транзакции
-	if (class==nil or security==nil or orderkey==nil) then
-		return "QL.killOrder(): Can`t kill order. Nil parameters."
-	end
-	if orderkey==0 then
-		return "QL.killOrder(): Can`t kill order. OrderKey=0."
+	if orderkey==nil or tonumber(orderkey)==0 then
+		return nil,"QL.killOrder(): Can`t kill order. OrderKey nil or zero"
 	end
 	local trans_id=math.random(2000000000)
 	local transaction={
 		["TRANS_ID"]=tostring(trans_id),
-		["CLASSCODE"]=class,
 		["SECCODE"]=security,
 		["ACTION"]="KILL_ORDER",
-		["ORDER_KEY"]=orderkey
+		["ORDER_KEY"]=tostring(orderkey)
 	}
+	if (security==nil and class==nil) or (class~=nil and security==nil) then
+		local order=getRowFromTable("orders","ordernum",orderkey)
+		transaction.class_code=order.class_code
+		transaction.seccode=order.seccode
+	elseif	security~=nil then
+		transaction.seccode=security
+		transaction.class_code=getSecurityInfo("",security).class_code
+	else
+		transaction.seccode=security
+		transaction.class_code=class 
+	end
 	local res=sendTransaction(transaction)
 	if res~="" then
-		return false,"QL.killOrder(): "..res
+		return nil,"QL.killOrder(): "..res
 	else
 		return true,"QL.killOrder(): Limit order kill sended. MAY NOT KILL!!! Class="..class.." Sec="..security.." Key="..orderkey.." Trans_id="..trans_id
 	end
@@ -245,10 +407,10 @@ function getHRDateTime()
 	local now=socket.gettime()
 	return string.format("%s,%3d",os.date("%c",now),select(2,math.modf(now))*1000)
 end
-function toPrice(class,security,value)
-	-- преобразования значения value к цене инструмента правильного формата
+function toPrice(security,value)
+	-- преобразования значения value к цене инструмента правильного ФОРМАТА (обрезаем лишнии знаки после разделителя)
 	-- Возвращает строку
-	local scale=getParamEx(class,security,"SEC_SCALE").param_value
+	local scale=getParamEx(getSecurityInfo("",security).class_code,security,"SEC_SCALE").param_value
 	local pos
 	_,pos=string.find(tostring(value),"%d+.")
 	if tonumber(scale)>0 then
@@ -260,14 +422,11 @@ function toPrice(class,security,value)
 end
 function getPosFromTable(table,value)
 	-- Возвращает ключ значения value из таблицы table, иначе -1
-	toLog("temp.txt","val="..value)
 	if (table==nil or value==nil) then
-		toLog("temp.txt","nil")
 		return -1
 	else
 		local k,v
 		for k,v in pairs(table) do
-			toLog("temp.txt","v="..v.." val="..value)
 			if v==value then
 				return k
 			end
@@ -317,4 +476,15 @@ function bit_set( flags, index )
     else
        return false
     end
+end
+function getRowFromTable(table_name,key,value)
+	-- возвращаем строку (таблицу Луа) из таблицы table_name с столбцом key равным value.
+	-- table_name[key].value
+	local i
+	for i=0,getNumberOf(table_name),1 do
+		if getItem(table_name,i)[key]==value then
+			return getItem(table_name,i)
+		end
+	end
+	return nil
 end
