e36d5a5dd3df0661f23764874124ef4f334d02c5
dsua <ys@ds-ua.com>
2013-02-08 10:17:01 +0200

    test Signed-off-by: dsua <ys@ds-ua.com>

--- QL.lua
+++ QL.lua
@@ -77,7 +77,7 @@ function sendMarket(class,security,direction,volume,account,client_code,comment)
 		["SECCODE"]=security,
 		["OPERATION"]=direction,
 		["TYPE"]="M",
-		["QUANTITY"]=volume,
+		["QUANTITY"]=tostring(volume),
 		["ACCOUNT"]=account
 	}
 	if comment~=nil then
@@ -578,4 +578,59 @@ function getRowFromTable(table_name,key,value)
 		end
 	end
 	return nil
+end
+function HiResTimer()
+-- переделка модуля http://lua-users.org/wiki/HiResTimer
+-- предоставляет оооочень точный таймер
+	local alien=require"alien"
+
+	--
+	-- get the kernel dll
+	--
+	local kernel32=alien.load("kernel32.dll")
+
+	--
+	-- get dll functions
+	--
+	local QueryPerformanceCounter=kernel32.QueryPerformanceCounter
+	QueryPerformanceCounter:types{ret="int",abi="stdcall","pointer"}
+	local QueryPerformanceFrequency=kernel32.QueryPerformanceFrequency
+	QueryPerformanceFrequency:types{ret="int",abi="stdcall","pointer"}
+	--------------------------------------------
+	--- utility : convert a long to an unsigned long value
+	-- (because alien does not support longlong nor ulong)
+	--------------------------------------------
+	local function lu(long)
+		return long<0 and long+0x80000000+0x80000000 or long
+	end
+
+	--------------------------------------------
+	--- Query the performance frequency.
+	-- @return (number)
+	--------------------------------------------
+	local function qpf()
+		local frequency=alien.array('long',2)
+		QueryPerformanceFrequency(frequency.buffer)
+		return  math.ldexp(lu(frequency[1]),0)
+				+math.ldexp(lu(frequency[2]),32)
+	end
+
+	--------------------------------------------
+	--- Query the performance counter.
+	-- @return (number)
+	--------------------------------------------
+	local function qpc()
+		local counter=alien.array('long',2)
+		QueryPerformanceCounter(counter.buffer)
+		return	 math.ldexp(lu(counter[1]),0)
+				+math.ldexp(lu(counter[2]),32)
+	end
+
+	--------------------------------------------
+	-- get the startup values
+	--------------------------------------------
+	local f0=qpf()
+	local c0=qpc()
+	local c1=qpc()
+	return (c1-c0)/f0
 end
